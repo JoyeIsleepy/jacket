6c1538e5ac1efb70a35de2d35ba40da634c5aa93
joey <joey_1999@163.com>
2026-01-26 22:08:33 +0800

    feat: 修改

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // 关键：禁用 Rolldown，改用 Esbuild
+  experimental: {
+    rolldown: false,
+  },
+});
